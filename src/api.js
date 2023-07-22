8431cc27aebbcdd72bc0d222cf91334511cb38d3
Shivam SINGH <shivam2003sy@gmail.com>
2023-07-22 20:35:49 +0000

    axios import

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+
 export const getPlacesData = async (type, sw, ne) => {
   try {
     const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
